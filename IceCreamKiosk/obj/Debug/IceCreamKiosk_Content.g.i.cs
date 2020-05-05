d6e9cfe800cecd28ed8ab0b6aceb21c6dada856c
ehud166 <ehud166@gmail.com>
2020-05-05 21:41:18 +0300

    fixed main step 2

--- IceCreamKiosk/obj/Debug/IceCreamKiosk_Content.g.i.cs
+++ IceCreamKiosk/obj/Debug/IceCreamKiosk_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("no-image-icon.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("sun1.jpg")]
 
 
