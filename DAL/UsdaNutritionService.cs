bc62a4bb1e49911fd5456e1a6db4e6db7406320c
ehud166 <45228315+ehud166@users.noreply.github.com>
2020-05-17 19:00:10 +0300

    Update UsdaNutritionService.cs
    clean

--- DAL/UsdaNutritionService.cs
+++ DAL/UsdaNutritionService.cs
@@ -35,56 +35,7 @@ namespace DAL
             return nutrients;
             }
             catch { throw; }
-            
-            //Nutritions nutrients = new Nutritions();
-            //string url = "https://api.nal.usda.gov/ndb/V2/";
-            //using (var client = new HttpClient())
-            //{
-            //    client.BaseAddress = new Uri(url);
-            //    client.DefaultRequestHeaders.Accept.Clear();
-            //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(@"application/json"));
-
-            //    HttpResponseMessage response = await client.GetAsync(string.Format("reports?ndbno={0}&type=f&format=json&api_key={1}", GetProductID(productName), API_KEY));
-            //    HttpContent content = response.Content;
-            //    string result = await content.ReadAsStringAsync();
-
-            //    JObject jobject = JObject.Parse(result);
-            //    var nutrientsList = jobject["foods"][0]["food"]["nutrients"];
-            //    foreach (var item in nutrientsList)
-            //    {
-            //        string nutrientId = item["nutrient_id"].ToString();
-            //        double nutrientValue = double.Parse(item["value"].ToString());
-            //        string nutrientName = item["name"].ToString();
-
-            //        switch (nutrientId)
-            //        {
-            //            case "203":
-            //                nutrients.Protein = nutrientValue;
-            //                break;
-            //            case "204":
-            //                nutrients.TotalFat = nutrientValue;
-            //                break;
-            //            case "205":
-            //                nutrients.Carbohydrate = nutrientValue;
-            //                break;
-            //            case "208":
-            //                nutrients.Energy = nutrientValue;
-            //                break;
-            //            case "269":
-            //                nutrients.Sugars = nutrientValue;
-            //                break;
-            //            case "291":
-            //                nutrients.Fiber = nutrientValue;
-            //                break;
-            //            case "601":
-            //                nutrients.Cholesterol = nutrientValue;
-            //                break;
-            //            default:
-            //                break;
-            //        }
-            //    }
-            //}
-            //return nutrients;
+         
         }
 
         public string GetProductID(string foodParms)
